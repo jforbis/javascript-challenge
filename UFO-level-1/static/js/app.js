0c187b4435f57cd5ce3431740eaf73da20cd8320
jforbis <forbisjohn@gmail.com>
2021-02-13 15:58:24 -0600

    save point: finished part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,26 +24,16 @@ let button = d3.select("#filter-btn");
 
 function handleClick() {
     d3.event.preventDefault();
+    tbody.html("");
     let inputField = d3.select("#datetime");
     let inputValue = inputField.property("value");
     // console.log(inputValue);
     let filteredData = tableData.filter(date => date.datetime === inputValue);
     console.log(filteredData);
-    // buildtable(filteredData)
+    if (filteredData.length === 0) {
+        buildtable(tableData)
+    }
+        else buildtable(filteredData)
 }
 
 button.on("click", handleClick);
-
-// // Build filtered table
-// function buildFilteredTable() {
-//     filteredData.forEach(function(siting) {
-//       // console.log(siting);
-//       let row = tbody.append("tr");
-      
-//       Object.entries(siting).forEach(function([key, value]) {
-//           // console.log(key, value);
-//           let cell = row.append("td");
-//           cell.text(value);
-//         });
-//     });
-//   }
