1c2fd47343893fa53b23d3669a896ca5ca88aa19
jforbis <forbisjohn@gmail.com>
2021-02-13 17:04:59 -0600

    save point: added filter by boxes and updated index.html file

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,48 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// creating my table
+let tbody = d3.select("tbody");
+
+// console.log(tableData);
+function buildtable(table) {
+  table.forEach(function(siting) {
+    // console.log(siting);
+    let row = tbody.append("tr");
+    
+    Object.entries(siting).forEach(function([key, value]) {
+        // console.log(key, value);
+        let cell = row.append("td");
+        cell.text(value);
+      });
+  });
+}
+buildtable(tableData);
+
+// listener and function to run when the site's button is clicked
+let button = d3.select("#filter-btn");
+let button2 = d3.select("#reset-btn");
+
+function handleClick() {
+    d3.event.preventDefault();
+    tbody.html("");
+    let inputField = d3.select("#datetime");
+    let inputValue = inputField.property("value");
+    // console.log(inputValue);
+    let filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+    if (filteredData.length === 0) {
+        buildtable(tableData)
+    }
+        else buildtable(filteredData)
+}
+
+function resetClick() {
+  tbody.html("");
+  buildtable(tableData)
+}
+
+button.on("click", handleClick);
+button2.on("click", resetClick);
+
+// Filter based on: date, city, state, country, shape
